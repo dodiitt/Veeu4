144ff444f44f94196819554e705914856e69fa1b
dodiitt <dodhie.virgo4@gmail.com>
2018-11-01 00:58:45 +0700

    Create vip.php

---
+++ vip.php
@@ -0,0 +1,1047 @@
+/* Decoded by Jack'n'Jill */
+
+<?php $ {
+    "GLOBALS"
+}
+["rppwle"] = "turkis";
+$ {
+    "GLOBALS"
+}
+["ozhdzhjuqm"] = "tpoin";
+$ {
+    "GLOBALS"
+}
+["hjvfncish"] = "second";
+$ {
+    "GLOBALS"
+}
+["gciigh"] = "jvid";
+$ {
+    "GLOBALS"
+}
+["haqjbezwtre"] = "rpoin";
+$ {
+    "GLOBALS"
+}
+["hunilbswnj"] = "msg";
+$ {
+    "GLOBALS"
+}
+["tqwgkdpgw"] = "vinfo";
+$ {
+    "GLOBALS"
+}
+["fqiqdtffn"] = "vhttp";
+$ {
+    "GLOBALS"
+}
+["vdkrnykoidd"] = "header";
+$ {
+    "GLOBALS"
+}
+["abfeuk"] = "konfir";
+$ {
+    "GLOBALS"
+}
+["fkjnalv"] = "earn";
+$ {
+    "GLOBALS"
+}
+["qqqrbbeyuls"] = "pcode";
+$ {
+    "GLOBALS"
+}
+["mmhtluk"] = "js";
+$ {
+    "GLOBALS"
+}
+["yklfldlhi"] = "lpoin";
+$ {
+    "GLOBALS"
+}
+["iwubhgkpyhn"] = "ch";
+$oesvize = "ch";
+$ {
+    "GLOBALS"
+}
+["bseinyhrik"] = "red";
+$ {
+    "GLOBALS"
+}
+["txniqoiut"] = "email";
+$ {
+    "GLOBALS"
+}
+["miknrswr"] = "invite";
+$ {
+    "GLOBALS"
+}
+["bditqkdgmk"] = "uid";
+$ {
+    "GLOBALS"
+}
+["jfmliyqkqv"] = "user";
+$ {
+    "GLOBALS"
+}
+["otwewgry"] = "http";
+$ {
+    "GLOBALS"
+}
+["qkzuumgkojl"] = "result";
+$ {
+    "GLOBALS"
+}
+["rkonvnr"] = "jres";
+$ {
+    "GLOBALS"
+}
+["xwmaeugvkll"] = "info";
+$ {
+    "GLOBALS"
+}
+["dfwqhrn"] = "access_token";
+$ {
+    "GLOBALS"
+}
+["jfxbmbehrevb"] = "pcode";
+$ {
+    "GLOBALS"
+}
+["qajrktmxr"] = "earn";
+$ {
+    "GLOBALS"
+}
+["uyllmpi"] = "curl";
+$ {
+    "GLOBALS"
+}
+["lxrbwmtffgc"] = "email";
+$ {
+    "GLOBALS"
+}
+["xpivgmirdgk"] = "head";
+$qejwhiicd = "pinfo";
+$ {
+    "GLOBALS"
+}
+["lyxopjxorfi"] = "video";
+$ {
+    "GLOBALS"
+}
+["esqiepoy"] = "link";
+$ {
+    "GLOBALS"
+}
+["xsbepanksk"] = "putih";
+$ {
+    "GLOBALS"
+}
+["sehhepb"] = "ch";
+$ {
+    "GLOBALS"
+}
+["vuqdxzfzofi"] = "t";
+$ {
+    "GLOBALS"
+}
+["ktcmrjpgg"] = "kuning";
+$cwilbt = "pinfo";
+$ {
+    "GLOBALS"
+}
+["gympxomfxx"] = "ijo";
+$ {
+    "GLOBALS"
+}
+["zwllmooqrysu"] = "ijo";
+$ {
+    "GLOBALS"
+}
+["ulllnbw"] = "biru";
+$yhxbrunnkluc = "t";
+$ {
+    "GLOBALS"
+}
+["imqpetf"] = "ch";
+$fechbx = "turkis";
+@system("clear");
+$ {
+    "GLOBALS"
+}
+["ojbgwjoy"] = "red";
+$ {
+    "GLOBALS"
+}
+["jgntsehblf"] = "curl";
+$vdsvvt = "pink";
+$ {
+    "GLOBALS"
+}
+["lleqoojpla"] = "putih";
+$ {
+    "GLOBALS"
+}
+["axuccgfd"] = "putih";
+$ {
+    "GLOBALS"
+}
+["uisgibtwzn"] = "ijo";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ulllnbw"]
+} = "[1;34m";
+$ {
+    $fechbx
+} = "[1;36m";
+$bwltzjkybw = "head";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["zwllmooqrysu"]
+} = "[92m";
+$ {
+    "GLOBALS"
+}
+["wypwuqut"] = "jres";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["axuccgfd"]
+} = "[1;37m";
+$ {
+    "GLOBALS"
+}
+["gxfngkrscu"] = "jres";
+$ {
+    $vdsvvt
+} = "[1;35m";
+$ {
+    "GLOBALS"
+}
+["roiebxnsxa"] = "head";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ojbgwjoy"]
+} = "[1;31m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ktcmrjpgg"]
+} = "[1;33m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["vuqdxzfzofi"]
+} = "
+";
+$tdhaqebtcem = "js";
+$ifzjhxmg = "access_token";
+$fwvtmngl = "header";
+print $ {
+    $ {
+        "GLOBALS"
+    }
+    ["gympxomfxx"]
+} . "
+__     _______ _____ _   _ 
+\ \   / / ____| ____| | | | " . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["xsbepanksk"]
+} . "versi: 4 VIP Reward included" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["zwllmooqrysu"]
+} . "
+ \ \ / /|  _| |  _| | | | | " . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["xsbepanksk"]
+} . "spesial VIP Reward" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["zwllmooqrysu"]
+} . "
+  \ V / | |___| |___| |_| | " . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["lleqoojpla"]
+} . "double reward" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["uisgibtwzn"]
+} . "
+   \_/  |_____|_____|\___/ " . $ {
+    $yhxbrunnkluc
+};
+$bvudghjcjxg = "body0";
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["xsbepanksk"]
+} . "by : ≠Jack'n'Jill | bonus invite: T5SDW8SY" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["vuqdxzfzofi"]
+} . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["vuqdxzfzofi"]
+};
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["esqiepoy"]
+} = "https://www.veeuapp.com/v1.0/incentive/tasks?access_token=" . $ {
+    $ifzjhxmg
+};
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["lyxopjxorfi"]
+} = array("locale" => "in_ID", "task_extra_info" => "", "task_name" => "watch_video_ball_01", "timezone" => "GMT+07:00");
+$ {
+    $bvudghjcjxg
+} = json_encode($ {
+    $ {
+        "GLOBALS"
+    }
+    ["lyxopjxorfi"]
+}, true);
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["xpivgmirdgk"]
+} = array();
+$ {
+    $bwltzjkybw
+}
+[] = "Host: www.veeuapp.com";
+"Connection: Keep-Alive";
+$ {
+    "GLOBALS"
+}
+["xiykprmdwu"] = "info";
+"Accept-Encoding: gzip";
+$qguydorc = "header";
+"User-Agent: okhttp/3.10.0";
+$ {
+    $qguydorc
+} = array();
+$ {
+    "GLOBALS"
+}
+["epvybnjag"] = "jres";
+$ioyqkvy = "total";
+$dxlodlptvwrq = "result";
+$ {
+    $fwvtmngl
+}
+[] = "Content-Type: application/json";
+"charset=UTF-8";
+"Content-Length: 96";
+"Host: www.veeuapp.com";
+$ {
+    "GLOBALS"
+}
+["wxuhwnckmeb"] = "konfir";
+$dqiuymijdwk = "result";
+"Connection: Keep-Alive";
+"Accept-Encoding: gzip";
+"User-Agent: okhttp/3.10.0";
+require ("config.php");
+$lktutag = "putih";
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["ktcmrjpgg"]
+} . "[>] $putih sedang login.....
+";
+sleep(1);
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["uyllmpi"]
+} = curl_init();
+curl_setopt_array($ {
+    $ {
+        "GLOBALS"
+    }
+    ["uyllmpi"]
+}, array(CURLOPT_URL => "https://www.veeuapp.com/v1.0/me?access_token=" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["dfwqhrn"]
+}, CURLOPT_RETURNTRANSFER => 1, CURLOPT_TIMEOUT => 30, CURLOPT_HTTPHEADER => $ {
+    $ {
+        "GLOBALS"
+    }
+    ["roiebxnsxa"]
+}, CURLOPT_SSL_VERIFYPEER => 0,));
+$ {
+    $dqiuymijdwk
+} = curl_exec($ {
+    $ {
+        "GLOBALS"
+    }
+    ["uyllmpi"]
+});
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["xwmaeugvkll"]
+} = curl_getinfo($ {
+    $ {
+        "GLOBALS"
+    }
+    ["jgntsehblf"]
+});
+curl_close($ {
+    $ {
+        "GLOBALS"
+    }
+    ["uyllmpi"]
+});
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["rkonvnr"]
+} = json_decode($ {
+    $ {
+        "GLOBALS"
+    }
+    ["qkzuumgkojl"]
+}, true);
+$pufcpmngpr = "jres";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["otwewgry"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["xiykprmdwu"]
+}
+["http_code"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["jfmliyqkqv"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["wypwuqut"]
+}
+["user"]["nickname"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["lxrbwmtffgc"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["gxfngkrscu"]
+}
+["user"]["email"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["bditqkdgmk"]
+} = $ {
+    $pufcpmngpr
+}
+["user"]["user_id"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["miknrswr"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["epvybnjag"]
+}
+["user"]["invite_code"];
+$blkvgmocymq = "lpoin";
+if ($ {
+    $ {
+        "GLOBALS"
+    }
+    ["otwewgry"]
+} == "200") {
+    $ {
+        "GLOBALS"
+    }
+    ["dpfdrlvlp"] = "ijo";
+    $istmnyuui = "putih";
+    $tkwwmqdmbj = "user";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["zwllmooqrysu"]
+    } . "[*] " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . "login: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["dpfdrlvlp"]
+    } . "sukses !!!
+";
+    echo $ {
+        $istmnyuui
+    } . "user: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ulllnbw"]
+    } . $ {
+        $tkwwmqdmbj
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . " | email: $biru" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["txniqoiut"]
+    } . "
+";
+} else {
+    $ {
+        "GLOBALS"
+    }
+    ["mhermp"] = "kuning";
+    $ {
+        "GLOBALS"
+    }
+    ["gdeacscico"] = "putih";
+    $ {
+        "GLOBALS"
+    }
+    ["iitskwtqtu"] = "t";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . "login: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["bseinyhrik"]
+    } . "failed !" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["iitskwtqtu"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["gdeacscico"]
+    } . " | message: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["mhermp"]
+    } . "token salah atau jaringan buruk" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["vuqdxzfzofi"]
+    };
+    exit;
+}
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["iwubhgkpyhn"]
+} = curl_init();
+$ {
+    $blkvgmocymq
+} = "https://www.veeuapp.com/v1.0/incentive/point?access_token=" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["dfwqhrn"]
+} . "&timezone=GMT%2B07%3A00&locale=in_ID";
+curl_setopt($ {
+    $ {
+        "GLOBALS"
+    }
+    ["sehhepb"]
+}, CURLOPT_URL, $ {
+    $ {
+        "GLOBALS"
+    }
+    ["yklfldlhi"]
+});
+curl_setopt($ {
+    $ {
+        "GLOBALS"
+    }
+    ["iwubhgkpyhn"]
+}, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ {
+    $ {
+        "GLOBALS"
+    }
+    ["iwubhgkpyhn"]
+}, CURLOPT_SSL_VERIFYPEER, 0);
+$ {
+    $dxlodlptvwrq
+} = curl_exec($ {
+    $ {
+        "GLOBALS"
+    }
+    ["imqpetf"]
+});
+$ {
+    $qejwhiicd
+} = curl_getinfo($ {
+    $oesvize
+});
+curl_close($ {
+    $ {
+        "GLOBALS"
+    }
+    ["iwubhgkpyhn"]
+});
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["mmhtluk"]
+} = json_decode($ {
+    $ {
+        "GLOBALS"
+    }
+    ["qkzuumgkojl"]
+}, true);
+$ {
+    $ioyqkvy
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["mmhtluk"]
+}
+["cumulative_point"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qajrktmxr"]
+} = $ {
+    $tdhaqebtcem
+}
+["today_revenue_point"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qqqrbbeyuls"]
+} = $ {
+    $cwilbt
+}
+["http_code"];
+if ($ {
+    $ {
+        "GLOBALS"
+    }
+    ["jfxbmbehrevb"]
+} == "200") {
+    $ {
+        "GLOBALS"
+    }
+    ["hrjpcsfi"] = "t";
+    $ {
+        "GLOBALS"
+    }
+    ["ovqutmarhhz"] = "ijo";
+    $wqyagix = "total";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . "total poin: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["zwllmooqrysu"]
+    } . $ {
+        $wqyagix
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . " | today earn: " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ovqutmarhhz"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["fkjnalv"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["hrjpcsfi"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["vuqdxzfzofi"]
+    };
+}
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["abfeuk"]
+} = readline($ {
+    $lktutag
+} . "[?] $turkis konfirmasi (y/n): ");
+if ($ {
+    $ {
+        "GLOBALS"
+    }
+    ["abfeuk"]
+} == "y" OR $ {
+    $ {
+        "GLOBALS"
+    }
+    ["wxuhwnckmeb"]
+} == "Y") {
+    sleep(1);
+    $oyhdvkfdngxc = "kuning";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ktcmrjpgg"]
+    } . "[>] $putih memproses data !!!
+";
+    sleep(1);
+    echo $ {
+        $oyhdvkfdngxc
+    } . "[>] $putih mejalankan bot......
+";
+    sleep(1);
+} else {
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["bseinyhrik"]
+    } . "[!] " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xsbepanksk"]
+    } . "login dibatalkan
+";
+    exit;
+}
+require __DIR__ . "/modul/modules";
+while (true) {
+    $ {
+        "GLOBALS"
+    }
+    ["tbbyubfc"] = "body0";
+    $ {
+        "GLOBALS"
+    }
+    ["lxodsmivgq"] = "cpoin";
+    $zxgoaqs = "result_video";
+    $ {
+        "GLOBALS"
+    }
+    ["ptlwssqry"] = "result_video";
+    $ulspfotr = "link";
+    $ {
+        "GLOBALS"
+    }
+    ["yrhrrbcdejus"] = "jvid";
+    $olqriznbhdv = "vhttp";
+    $ {
+        "GLOBALS"
+    }
+    ["vsozbcrqjp"] = "curl";
+    sleep(1);
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["uyllmpi"]
+    } = curl_init();
+    $urrkbkrg = "msg";
+    $gqtlrnpby = "jvid";
+    curl_setopt_array($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vsozbcrqjp"]
+    }, array(CURLOPT_RETURNTRANSFER => 1, CURLOPT_URL => $ {
+        $ulspfotr
+    }, CURLOPT_TIMEOUT => 30, CURLOPT_POST => true, CURLOPT_POSTFIELDS => $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tbbyubfc"]
+    }, CURLOPT_HTTPHEADER => $ {
+        $ {
+            "GLOBALS"
+        }
+        ["vdkrnykoidd"]
+    }, CURLOPT_SSL_VERIFYPEER => 0,));
+    $bijzgfpvqgl = "vinfo";
+    $jirgcaqwf = "jvid";
+    $ {
+        "GLOBALS"
+    }
+    ["lcxytxsigbv"] = "jvid";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ptlwssqry"]
+    } = curl_exec($ {
+        $ {
+            "GLOBALS"
+        }
+        ["uyllmpi"]
+    });
+    $ {
+        $bijzgfpvqgl
+    } = curl_getinfo($ {
+        $ {
+            "GLOBALS"
+        }
+        ["uyllmpi"]
+    });
+    curl_close($ {
+        $ {
+            "GLOBALS"
+        }
+        ["uyllmpi"]
+    });
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lcxytxsigbv"]
+    } = json_decode($ {
+        $zxgoaqs
+    }, true);
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["fqiqdtffn"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tqwgkdpgw"]
+    }
+    ["http_code"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["hunilbswnj"]
+    } = $ {
+        $gqtlrnpby
+    }
+    ["message"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["haqjbezwtre"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["gciigh"]
+    }
+    ["task"]["reward_point"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lxodsmivgq"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["yrhrrbcdejus"]
+    }
+    ["point"]["current_point"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["hjvfncish"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["gciigh"]
+    }
+    ["task"]["extra"]["complete_seconds"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ozhdzhjuqm"]
+    } = $ {
+        $jirgcaqwf
+    }
+    ["point"]["today_revenue_point"];
+    if ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["hunilbswnj"]
+    } == "complete full") {
+        $ {
+            "GLOBALS"
+        }
+        ["sokrznmnyj"] = "red";
+        $grfoplfvarq = "msg";
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["sokrznmnyj"]
+        } . "[!] $putih nonton video:$red " . $ {
+            $grfoplfvarq
+        } . "$putih
+";
+        sleep(1);
+        break;
+    } elseif ($ {
+        $urrkbkrg
+    } == "task invalid") {
+        break;
+    } elseif ($ {
+        $olqriznbhdv
+    } == "200") {
+        $vlowgv = "putih";
+        $sbtayckumasd = "tpoin";
+        $pjevobmicvy = "turkis";
+        $ {
+            "GLOBALS"
+        }
+        ["whldiagex"] = "putih";
+        $jukwmtevt = "cpoin";
+        $dceovdhc = "kuning";
+        echo $ {
+            $dceovdhc
+        } . "[>] $ijo sukses $putih| user: $biru" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["jfmliyqkqv"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["rppwle"]
+        } . " | point: " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["whldiagex"]
+        } . $ {
+            $jukwmtevt
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["zwllmooqrysu"]
+        } . "
+[!] " . $ {
+            $pjevobmicvy
+        } . "reward_point: " . $ {
+            $vlowgv
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["haqjbezwtre"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["zwllmooqrysu"]
+        } . " | " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["rppwle"]
+        } . "today_revenue_point: " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xsbepanksk"]
+        } . $ {
+            $sbtayckumasd
+        } . "
+=================================================
+";
+    } else {
+        sleep(5);
+    }
+}
+require __DIR__ . "/modul/vip.php";
+?>
